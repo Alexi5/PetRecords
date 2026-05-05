0c6e555bf9d538513e5272632667003e8ae65102
Alexi5 <ajennings@Lexs-MBP.attlocal.net>
2026-05-05 14:28:54 -0500

    app port

--- server/index.js
+++ server/index.js
@@ -32,6 +32,8 @@ const app = express();
 app.use(express.json())
 app.use(cors());
 
+app.listen(3001, () => console.log('Server running on port 3001'));
+
 // ================ PETS endpoint ================
 app.get('/api/pets', (req, res) => {
     const pets = db.prepare('SELECT * FROM pets').all();
